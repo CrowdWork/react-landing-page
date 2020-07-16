6b0bf7f63cf6099be311852db69b504cc5a44721
caio_ingber <caioingber@gmail.com>
2020-07-16 16:30:08 -0400

    troubleshooting deployment errors

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.scss";
 import NavBar from "./Sections/NavBar/NavBar.js";
 import Donate from "./Sections/Donate/Donate.js";
+import Contribute from "./Sections/Contribute/Contribute.js";
 
 function App() {
   return (
     <div className="App">
-      {/* <Button type="Donate" /> */}
       <NavBar />
       <Donate />
     </div>
