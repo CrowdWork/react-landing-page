5f53ddb75ed236753f15fa982f2047bac742299e
caio_ingber <caioingber@gmail.com>
2020-08-05 19:43:37 -0400

    updated ignore

---
+++ build/precache-manifest.209593bc815e81ee12aff5cd87587546.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266d900806ab001bd867d0a07d5992f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddda8237d36da232287b",
+    "url": "/static/css/main.89fba312.chunk.css"
+  },
+  {
+    "revision": "c59f15e8567f4c9edc14",
+    "url": "/static/js/2.c341a934.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c341a934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddda8237d36da232287b",
+    "url": "/static/js/main.67bb8e21.chunk.js"
+  },
+  {
+    "revision": "8b72ba047bf8d34c8e5f",
+    "url": "/static/js/runtime-main.f53ad909.js"
+  },
+  {
+    "revision": "0ae49bb896d2b79458854626af2d38db",
+    "url": "/static/media/check.0ae49bb8.svg"
+  },
+  {
+    "revision": "541dcc715f5eb9299c7704b9d45c5672",
+    "url": "/static/media/crowdwork_logo.541dcc71.png"
+  },
+  {
+    "revision": "ae54bb0e1573c1c2899d8e2b90246f23",
+    "url": "/static/media/icons8-checkmark.ae54bb0e.svg"
+  },
+  {
+    "revision": "5e942d68e2f42ed84b905323c13a1983",
+    "url": "/static/media/sad.5e942d68.svg"
+  }
+]);
